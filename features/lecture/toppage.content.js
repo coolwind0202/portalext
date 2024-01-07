1af997c585a572db089962d3da397aaa3d59c20a
coolwind0202 <tororo_shidoro@yahoo.co.jp>
2024-01-07 14:37:52 +0900

    [feat] トップページの講義リンク自動設定の実装

---
+++ features/lecture/toppage.content.js
@@ -0,0 +1,15 @@
+const getLectureURL = (day, hour) => `${document.URL}-2.IBehaviorListener.0-timetablePanel-content-timetableAxisListView-${hour}-timetableBeanListList-${day}-course-0-lectureLink`;
+
+const attachHrefToTds = () => {
+  const rows = document.querySelectorAll("table > tbody > tr");
+  const cells = [...rows].map(tr => [...tr.children].slice(1));
+
+  cells.forEach((tr, x) => {
+    tr.forEach((td, y) => {
+      const a = td.querySelector("div > a");
+      if (a != null) a.href = getLectureURL(y, x);
+    });
+  });
+}
+
+setTimeout(attachHrefToTds, 1000);
