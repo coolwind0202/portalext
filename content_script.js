05bb4e0ecc388535490af8bd805c1eb46c4cd12d
coolwind0202 <tororo_shidoro@yahoo.co.jp>
2024-01-08 23:06:39 +0900

    [chore] Delete .js file about create URL link feature

--- content_script.js
+++
@@ -1,7 +0,0 @@
-elements = document.querySelectorAll("p");
-
-urlPattern = /https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_\+.~#?&\/=]*)/g;
-elements.forEach(element => {
-    console.log(Array.from(element.innerHTML.matchAll(urlPattern)));
-    element.innerHTML = element.innerHTML.replaceAll(urlPattern, `<a href="$&">$&</a>`);
-});
